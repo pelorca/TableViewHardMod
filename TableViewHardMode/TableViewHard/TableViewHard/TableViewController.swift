1d6268b815920f77b5de4a09398686581bf0c4a5
Eduardo Pelorca <pelorca@gmail.com>
2017-11-30 18:22:09 -0200

    Ajustes

--- TableViewHardMode/TableViewHard/TableViewHard/TableViewController.swift
+++ TableViewHardMode/TableViewHard/TableViewHard/TableViewController.swift
@@ -14,13 +14,17 @@ class TableViewController: UITableViewController {
     let maxRow = 10
     lazy var dataIcon: [Int: [Icon]] = [:]
     lazy var images: [Int: [NetworkImage]] = [:]
+    var delete: Bool =  false
     
-    
+    @IBOutlet var tableViewCustom: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.images = generateImage(maxSession,maxRow)
         self.dataIcon = generateIcons(maxSession, maxRow)
+        tableViewCustom.setEditing(true, animated: true)
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,20 +32,51 @@ class TableViewController: UITableViewController {
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return  self.dataIcon.count
+        return  self.images.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.dataIcon[section]?.count ?? 0
+        return self.images[section]?.count ?? 0
     }
     
+    //override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+      //  return true
+    //}
+    
+   
+    //override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+     //   return .none
+    //  }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return ""
     }
     
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+       
+        return true
+    }
     
     
+    
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            images[indexPath.section]?.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        var itemToMove = self.images[sourceIndexPath.section]?[sourceIndexPath.row]
+        self.images[sourceIndexPath.section]?.remove(at: sourceIndexPath.row)
+        self.images[sourceIndexPath.section]?.insert(itemToMove!, at: destinationIndexPath.row)
+       
+    }
+    
+   
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "imageCell", for: indexPath) as! TableViewCell
